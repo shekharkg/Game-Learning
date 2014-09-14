866d8c52d78caabd75ecc80b7a5c37c5cccc259c
ShekharKG <shekhar114@gmail.com>
2014-09-15 00:04:55 +0530

    StartingClass is added with etended applet and implements runnable, keylistner

---
+++ src/java/StartingClass.java
@@ -0,0 +1,92 @@
+package java;
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		setSize(800, 400);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		Frame frame = (Frame) this.getParent().getParent();
+	}
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+		Thread thread = new Thread(this);
+		thread.start();
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		super.stop();
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		repaint();
+		try {
+			Thread.sleep(17);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_LEFT:
+			break;
+		case KeyEvent.VK_RIGHT:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			break;
+		case KeyEvent.VK_DOWN:
+			break;
+		case KeyEvent.VK_LEFT:
+			break;
+		case KeyEvent.VK_RIGHT:
+			break;
+		case KeyEvent.VK_SPACE:
+			break;
+		}
+	}
+
+}
